dca1aecee087d02bd60f410391c7277c2409fc43
Jakob Schrettenbrunner <jakob.schrettenbrunner@telekom.de>
2021-03-01 14:43:31 +0100

    add ipam interface

---
+++ pkg/ipam/interfaces.go
@@ -0,0 +1,27 @@
+package ipam
+
+import (
+	"net"
+)
+
+// Manager is an interface to an IPAM backend.
+type Manager interface {
+	// GetClusterSubnet(clusterIdentifier string, spec NetworkSpec) (Subnet, error)
+
+	// GetOrAllocateIP assigns a single IP address within a subnet that's assigned to a cluster
+	// The identifier is used as the device name in the IPAM and the clusterIdentifier is used
+	// to identify the subnet.
+	GetOrAllocateIP(identifier string, clusterIdentifier string) (net.IP, error)
+
+	// ReleaseIP releases a single IP address within a subnet that's assigned to a cluster.
+	// The identifier is the device name within the IPAM and the clusterIdentifier is used
+	// to identify the subnet.
+	ReleaseIP(identifier string, clusterIdentifier string, addr net.IP) error
+}
+
+// type Subnet interface {
+// 	Identifier() string
+// 	Network() *net.IPNet
+// }
+
+// type NetworkSpec struct{}
